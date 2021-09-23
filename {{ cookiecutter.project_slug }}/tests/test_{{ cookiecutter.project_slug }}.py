049d8a647ae8f945a518b769183e44e4dde4e330
Mike Hill <mhilluniversal@gmail.com>
2021-09-23 15:48:31 -0400

    Add test-related files

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -0,0 +1,12 @@
+"""Tests for the {{ cookiecutter.project_slug }} module."""
+# import typing
+# use @typing.no_type_check decorator when using pytest built-in fixtures
+# import pytest
+
+from {{ cookiecutter.project_slug }}.replace_me import replace_me
+
+
+def test_{{ cookiecutter.project_slug }}() -> None:
+    """Placeholder test for {{ cookiecutter.project_slug }}.
+    """
+    replace_me()
