6757e91109cb1da3f66dc75ef0f983d8ed4814a2
Mike Hill <mhilluniversal@gmail.com>
2021-09-23 15:47:39 -0400

    Added the placeholder package files that contain the actual code

---
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/replace_me.py
@@ -0,0 +1,9 @@
+"""A placeholder module meant to be replaced."""
+
+
+def replace_me() -> str:
+    """Replace this docstring.
+
+    :returns: A static string indicating this is a placeholder function.
+    """
+    return "This function should be replaced"
